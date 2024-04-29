d0ba2b5d0aa0e81aa75fdafe9e35c7aa1b60709f
elainegt20 <elainegarcia@xyz-vgmxrlw17t.mynetworksettings.com>
2024-04-29 15:11:16 -0400

    Completed week 4 project

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,53 @@
 class Scooter {
-  // scooter code here
+
+  static nextSerial = 1; //this property does not reset each time you create an intance
+
+  constructor(station) {
+    this.station = station;
+    this.user = null; // Scooters are docked initially without a user.
+    this.serial = Scooter.nextSerial;//Assign current serial
+    Scooter.nextSerial+=1;  //increment for next usage.
+    this.charge = 100;
+    this.isBroken = false;
+  }
+  
+
+  rent(user) {
+    if (this.charge < 20) {
+      throw new Error("Scooter needs to charge")
+    }else if(this.isBroken){
+      throw new Error("Scooter needs repair");
+    }
+    this.station = null;
+    this.user = user;
+
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+  recharge() {
+    setInterval(() => {
+      if (this.charge<100) {
+        this.charge += 10;
+      }
+      console.log(`Current charge: ${this.charge}%`);
+    }, 1000);
+  }
+  requestRepair(){
+    setTimeout(()=> {
+      if(this.isBroken){
+        this.isBroken=false;
+        console.log('Repair completed')
+      }
+
+    },5000)
+  }
+  
 }
 
-module.exports = Scooter
+const test= new Scooter('station a')
+console.log(typeof test.station)
+
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,23 +1,113 @@
-const Scooter = require('../src/Scooter')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User.js");
+
+let stationTestA = "Station A";
+let stationTestB = "Station B";
+const userTest = new User("Juan Perez", "jp1234", 25);
 
 // typeof scooter === object
-describe('scooter object', () => {
-  test('Scooter class should create Scooter instance', () => {
-    const scooter = new Scooter()
-    expect(scooter).toBeInstanceOf(Scooter)
-  })
-})
+describe("scooter object", () => {
+  let scooter;
+  beforeEach(() => {
+    scooter = new Scooter(stationTestA);
+    // Reset serial number before each test 
+    Scooter.nextSerial = 1;
+  });
+  test("Scooter class should create Scooter instance", () => {
+    
+    expect(scooter).toBeInstanceOf(Scooter);
+  });
+  test("Constructor should properly initialize properties", () => {
+    
+    expect(scooter.station).toBe(stationTestA);
+    expect(scooter.user).toBeNull();
+    expect(scooter.serial).toBe(1);
+    expect(scooter.charge).toBe(100);
+    expect(scooter.isBroken).toBe(false);
+    //expect(typeof scooter.station).toBe("string")
+  });
+  //test rent method: it set this.station to null and this.user to a User instance when this.charge is greater than 20 and thi.isBroken is false
+  test("Scooter rents correctly when charged and not broken", () => {
+    /*
+   The use of an arrow function ensures that any exceptions thrown can be caught by the testing framework. 
+   If you called scooter.rent(userTest) directly without wrapping it in a function, any thrown exceptions would occur 
+   before expect could handle them, causing the test to fail incorrectly.
+   */
+    
+    expect(() => scooter.rent(userTest)).not.toThrow();
+    expect(scooter.station).toBeNull();
+    expect(scooter.user).toBe(userTest);
+  });
+  test("Scooter rent throws error when charge is too low", () => {
+    
+    scooter.charge = 10; // set charge bellow 20
+    expect(() => scooter.rent(userTest)).toThrow("Scooter needs to charge");
+  });
+  test("Scooter rent throws error when scooter needs repair", () => {
+    
+    scooter.isBroken = true;
+    expect(() => scooter.rent(userTest)).toThrow("Scooter needs repair");
+  });
+  //test dock method: Returns the scooter to the station. Clear out the user
+  test("Scooter gets back to station correctly", () => {
+    
+    scooter.rent(stationTestA);
+    scooter.dock(stationTestB); //dock to another station
+    expect(scooter.station).toBe(stationTestB);
+    expect(scooter.user).toBeNull();
+  });
+  test("serial number properly increments", () => {
+    const scooter = new Scooter(stationTestA);
+    const scooter2 = new Scooter(stationTestB);
+    expect(scooter2.serial).toBe(2);
+  });
+});
 
-// Method tests
-describe('scooter methods', () => {
-  // tests here!
+describe("test asyn methods", () => {
+  jest.useFakeTimers(); // Use fake timers to control setInterval
+  let scooter;
+  let consoleSpy;
+  beforeEach(() => {
+    // Spy on console.log
+    consoleSpy = jest.spyOn(console, "log").mockImplementation();
+    scooter = new Scooter();
+  });
 
-  // rent method
+  afterEach(() => {
+    // Restore the spy
 
-  // dock method
+    consoleSpy.mockRestore();
+    //clear time
+    jest.clearAllTimers();
+  });
 
-  // requestRepair method
+  //test recharge method
+  test("Should adds 10 to charge every second if charge is bellow 100 and logs current charge to the console", () => {
+    scooter.charge = 20; //set charge bellow 100
+    scooter.recharge(); //call recharge method
+    jest.advanceTimersByTime(1000);
+    expect(scooter.charge).toBe(30);
+    expect(consoleSpy).toHaveBeenCalledWith(
+      `Current charge: ${scooter.charge}%`
+    );
 
-  // charge method
+    jest.advanceTimersByTime(3000); // Total 4 seconds from the start
+    expect(scooter.charge).toBe(60);
+    expect(consoleSpy).toHaveBeenCalledWith(
+      `Current charge: ${scooter.charge}%`
+    );
 
-})
+    //if charge is equal 100 it does not add more
+    scooter.charge = 100;
+    scooter.recharge();
+    expect(scooter.charge).toBe(100); //charge stays as it was
+  });
+  //test repair method
+  test("Should set isBroken property and logs a message to the console when certain time is reached", () => {
+    scooter.isBroken = true;
+    scooter.requestRepair();
+    jest.advanceTimersByTime(5000);
+    expect(scooter.isBroken).toBe(false);
+    expect(consoleSpy).toHaveBeenCalledWith("Repair completed");
+  });
+});
